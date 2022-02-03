e45211ed13beda3f5ccc5ce50e39c01cc0ceddea
mlapshin <mlapshin@uwaterloo.ca>
2022-02-02 22:32:44 -0500

    Added basic gitignore and Cpp mains used in setting up VSCode.

---
+++ the-holy-cow/main.cpp
@@ -0,0 +1,9 @@
+#include <cstdlib>
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    cout << "Hello the Holy Cow!" << endl;
+    return 0;
+}
---
+++ the-keep/main.cpp
@@ -0,0 +1,10 @@
+
+#include <cstdlib>
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    cout << "Hello the Keep!" << endl;
+    return 0;
+}
