6f5198cd63309a19d243a3c4ef4774b6a231ac58
Igor Lapshin <Igor.Lapshin@iritronix.ca>
2023-04-29 22:44:02 +0000

    Messaging client improvement

--- the-keep/core/task-manager/MessageClient.hpp
+++ the-keep/core/task-manager/MessageClient.hpp
@@ -11,6 +11,10 @@
 #include "comms/TheKeepDB.hpp"
 #include "comms/TheKeepMessaging.hpp"
 
+// Implementation specific drivers; a bit deviation from decoupled design, but much simpler
+#include "comms/KafkaDriver.hpp"
+#include "comms/CassandraDriver.hpp"
+
 namespace thekeep {
     using namespace std;
 
@@ -20,23 +24,42 @@ namespace thekeep {
     // and return them one by one
     // it's initialized from outside with a list? or single topic
 
-    class MessageClient 
-    {       
+    class MessageClient
+    {
         TheKeepMessaging *km;
-        TheKeepDB* kdb; 
+        TheKeepDB *kdb;
         queue<string> messages; // FIFO queue
 
-      public:
-        // TODO: receive / sent topics constructor. Add built-in initialization
-        MessageClient(TheKeepMessaging *km, TheKeepDB *kdb): km(km), kdb(kdb)
+    public:
+    MessageClient() {}
+        /// @brief a single receive / sent topic constructor with built-in initialization
+        ///        Assumes global Kafka and Database configuration from CommsConfig.hpp
+        /// @param topic - a topic to which will be subcsription
+        MessageClient(string topic) : MessageClient(vector<string> {topic}) {}
+
+        /// @brief receive / sent topics constructor with built-in initialization
+        ///        Assumes global Kafka and Database configuration from CommsConfig.hpp
+        /// @param topics - topics to which will be subcsription
+        MessageClient(const vector<string>& topics)
+        {
+            // vector<string> topics{KAFKA_CONTROL_TOPIC, KAFKA_DATA_TOPIC};
+            TheKeepMessaging *km = new KafkaDriver(KAFKA_URL, topics);
+            TheKeepDB *kdb = new CassandraDriver();
+            kdb->connect(CASSANRDA_URL, CASSANRDA_USER, CASSANRDA_PASSWORD);
+        }
+
+        /// @brief Proper De-coupled Constructor with implementation done outside
+        /// @param km   - initialized The Keep's DB
+        /// @param kdb  - initialized Kafka driver subscribed to required topics
+        MessageClient(TheKeepMessaging *km, TheKeepDB *kdb) : km(km), kdb(kdb)
         {
             // null pts checks just in case (change to reference?)
-            if(!km || !kdb) 
+            if (!km || !kdb)
             {
                 throw runtime_error("MessageClient initialized with a NULL object");
             }
 
-            km->subscribe(KAFKA_CONTROL_TOPIC);  // todo: another messaging object
+            km->subscribe(KAFKA_CONTROL_TOPIC); // todo: another messaging object
             km->subscribe(KAFKA_DATA_TOPIC);
         }
 
@@ -47,7 +70,7 @@ namespace thekeep {
             static auto q = km->receive();
             const string& msg=q.front();
             q.pop();
-            return msg; // picks up only the very frist messasge - chceck how to handle this TODO
+            return msg; // picks up only the very first messasge - chceck how to handle this TODO
         }
 
         const string pollFetchDataUpdate() const
@@ -61,16 +84,13 @@ namespace thekeep {
         const string fetchUserDataRequest() const
         {
             
-            
             return "";
         }
 
         void send() 
         {
-            //km->initialize("");
-            //km->subscribe("keepid-tests");
-            km->send("keepid-tests","test-c++ 1235");
-            km->send("keepid-tests","test-c++ 123567");
+            // km->send("keepid-tests","test-c++ 1235");
+            // km->send("keepid-tests","test-c++ 123567");
 
             queue<string> messages;
             for (int i = 0; i < 100; i++)
